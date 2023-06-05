226cc5b77cd1abd4d7d7149620ffa685a516a35b
Daniel Massita Tonolli <111195175+danielmassita@users.noreply.github.com>
2023-06-05 18:09:26 -0300

    Create Aula02a.py

---
+++ Aula02a.py
@@ -0,0 +1,21 @@
+# https://www.cursoemvideo.com/curso/mysql/aulas/banco-de-dados/modulos/instalando-o-mysql-com-wamp/
+# https://youtu.be/5JbAOWJbgIA
+
+"""
+- Em 1994, Suécia, o Michael Widenius e o David Axmark resolveram criar um modelo gratuito de banco de dados relacional (MySQL), gratuito e aberto licença GPL.
+- Nem tudo que é grátias é livre (open source).
+- Em 2007 a Sun Microsystems comprou o MySQL. A Sun foi a empresa que criou a linguagem Java.
+- Em 2009 a Sun Microsystems foi comprado pela Oracle. A Oracle, que já era uma gigante de dados, passou a ser também dona do MySQL. 
+- Então, chateado, Michael Widenius saiu do MySQL e criou o MariaDB (fork do MySQL). 
+- Empresas que usam o MySQL: Nasa, Google, Wikipedia, Adobe, Cisco, Ebay, Bradesco, Hostnet...
+
+- MySQL possui todos os padrões: 1) Base de Dados, 2) Linguagem de Especificação, 3) Sistema Gerenciador, 4) Programas Adicionais
+- 
+
+- Instruções do MySQL que se caracterizam como linguagens do tipo:
+    DDL - Definição da Estrutura da BD
+    DML - Manipulação (incluir, excluir, alterar)
+    DQL - Solicitações (SELECT, solicitações)
+    DCL - Controle (quais usuários podem acessar e etc)
+    DDL - Transação (solicitação feita ao BD executados da melhor maneira seguinda as Características de Transação DICA (Durabilidade, Isolamento, Consistência e Atomicidade))
+    
