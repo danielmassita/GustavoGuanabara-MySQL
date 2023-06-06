e0596b84541af68fd7d4a1bda4add920184fe5f7
Daniel Massita Tonolli <111195175+danielmassita@users.noreply.github.com>
2023-06-06 16:39:14 -0300

    Create Aula04.py

---
+++ Aula04.py
@@ -0,0 +1,10 @@
+# Curso MySQL #04 - Melhorando a Estrutura do Banco de Dados 
+
+# https://www.cursoemvideo.com/curso/mysql/aulas/banco-de-dados/modulos/melhorando-a-estrutura-do-banco-de-dados/ 
+# https://youtu.be/cHLKtALWDos
+
+"""
+Aula 04 - Curso MySQL #04 - Melhorando a Estrutura do Banco de Dados 
+
+Hello, world! 
+"""
