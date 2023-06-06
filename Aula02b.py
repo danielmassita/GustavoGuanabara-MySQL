5ad2c7e460732495ee69c59e6e3c153dbaa3b44f
Daniel Massita Tonolli <111195175+danielmassita@users.noreply.github.com>
2023-06-06 00:07:04 -0300

    Create Aula02b.py

---
+++ Aula02b.py
@@ -0,0 +1,698 @@
+# https://www.cursoemvideo.com/curso/mysql/aulas/banco-de-dados/modulos/instalando-o-xampp/
+# https://youtu.be/R2HrwSQ6EPM
+
+"""
+Transcrição
+
+
+0:19
+olá pequeno gafanhoto seja bem vindo a
+0:22
+mais uma aula do curso em vídeo de banco
+0:24
+de dados
+0:25
+meu nome é gustavo guanabara eu sou
+0:26
+professor e dessa vez a gente vai fazer
+0:28
+uma aula extra na verdade você vai ver
+0:30
+que a sala vai ser lançada como um extra
+0:32
+ela vai ser complementar a aula de
+0:35
+instalação do ambiente do mas que é ele
+0:37
+porque muitos alunos entrar em contato
+0:39
+com a gente dizendo que o observa que a
+0:42
+gente anunciou aqui que eu ensinei como
+0:43
+estava ele não estava funcionando em
+0:45
+alguns casos e eu vim apresentar uma
+0:47
+outra possibilidade é importante que se
+0:49
+diga o seguinte se você instalou absorvi
+0:52
+está funcionando sem problemas no seu
+0:54
+computador não precisa assistir essa
+0:55
+aula pode pular para a próxima sem
+0:57
+problema nenhum sem perder nenhuma o
+0:59
+motivo se agravando esse vídeo é pra
+1:01
+ajudar a galera que não conseguiu
+1:03
+instalar observa que está dando problema
+1:04
+na porta eu fiz algumas pesquisas
+1:05
+algumas experiências e cheguei a uma
+1:07
+outra possibilidade eu espero
+1:09
+sinceramente que esta funciona para você
+1:11
+então abriu se um ambiente abre seu
+1:13
+navegador
+1:14
+vamos começar a trabalhar vão baixar
+1:15
+essa nova solução aqui então vou abrir
+1:18
+pelo menos um navegador e vou acessar o
+1:21
+site www ponto a partir frentes ponto
+1:24
+org
+1:25
+no site as frentes você vai baixar a
+1:28
+última versão do cempre na verdade a
+1:30
+maioria das pessoas fala champ o certo
+1:32
+de falar exemplo mas nada impede que
+1:34
+você fala a chance sem problema nenhum
+1:35
+só não acho esquisito se alguém fazendo
+1:37
+o exemplo é exatamente a mesma coisa que
+1:40
+o mpe ele é um apache mais ql php ele
+1:44
+instala esses três coisas o apache que é
+1:46
+o serviço servidor o mais quer que o
+1:48
+serviço de banco de dados e um
+1:49
+interpretador php
+1:50
+esse x é de qualquer plataforma ele vai
+1:53
+conseguir rodar tanto no windows quanto
+1:54
+no méxico contra o linux a galera que
+1:56
+está sentindo necessidade da instalação
+1:57
+do ambiente para linux é só estava
+1:59
+ausente que funciona também para baixar
+2:01
+a versão mais estável baixo você
+2:02
+escolher qualquer uma delas o que eu
+2:04
+tenho o exemplo para windows exemplo
+2:06
+para linux exemplo prometeu s se você
+2:09
+clicar aqui ó em clique aqui para outras
+2:11
+versões
+2:11
+você vai ser desviado por uma outra
+2:12
+página
+2:13
+onde você vai poder baixar opções
+2:16
+específicas por exemplo se você pode
+2:18
+você pode baixar aqui ó com a versão 5.5
+2:20
+php 5.6 ou 7.0 que a última versão do
+2:24
+php
+2:24
+é claro que quando você tiver seja nesse
+2:25
+site pode ser que já tem uma versão mais
+2:27
+nova baixa sempre a versão mais estável
+2:30
+a minha recomendação assim por exemplo
+2:31
+no momento em que eu estou acessando o
+2:33
+site agora ele tá me disponibilizando
+2:35
+aqui a 7.0
+2:36
+mas ela não é ainda uma versão
+2:37
+totalmente estável então eu vou voltar à
+2:39
+página inicial e vou baixar a versão
+2:41
+estável fique livre para baixar qualquer
+2:44
+uma delas inclusive uma delas não
+2:45
+funcionar
+2:46
+você ainda tem a opção de baixar outro
+2:47
+eu vou por opção a voltar aqui para a
+2:49
+página anterior e vou fazer o download
+2:51
+da versão atual aqui na versão que é
+2:53
+sugerida o download já vai começar
+2:56
+ele vai segundo aquele vai levar uns 25
+2:58
+minutos
+2:59
+o download é realmente um pouquinho
+3:01
+grande no meu caso aqui nessa versão
+3:02
+contou baixando um pouco mais de 100
+3:04
+mega e assim que o download terminar
+3:06
+você pode acessar diretamente a sua
+3:07
+pasta de transferência e executar o
+3:09
+arquivo que acabou de ser a chave então
+3:11
+terminado download você pode lá no local
+3:13
+onde foi baixado
+3:14
+você vai ter o seu arquivo lá de
+3:15
+instalação ou clicar duas vezes nele o
+3:19
+processo de instalação vai ser disparado
+3:21
+aqui vai abrir a janela estava dizendo
+3:23
+que estou utilizando o user account
+3:24
+control que é o controle do usuário no
+3:26
+meu caso aqui o meu usuário é um
+3:27
+administrador então ele tem todas as
+3:28
+permissões
+3:29
+é importante que você faça obviamente a
+3:31
+instalação do seu servidor como um
+3:33
+administrador
+3:34
+você não pode instalar numa máquina que
+3:35
+você não tem privilégios de
+3:37
+administrador é pra você pode instalar
+3:39
+ferramentas e instalar e configurar
+3:40
+ambientes um erro que pode ser comum é
+3:43
+você está tentando instalar o exemplo o
+3:45
+web utilizando o privilégio de usuário
+3:47
+comum então deixando bem claro você tem
+3:49
+que estar logado como usuário
+3:50
+administrador
+3:51
+se você não for usuário administrador
+3:53
+por exemplo o seu pai o seu irmão pede
+3:55
+para ele entrar como administrador para
+3:57
+você poder fazer a instalação explica
+3:59
+para ele a ferramenta para o estudo mas
+4:00
+com certeza ele vai utilizar então a
+4:03
+estação base é sempre a mesma você vai
+4:04
+dando next
+4:05
+eu vou fazer a opção aqui que instale
+4:08
+todas as configurações next
+4:10
+vou manter a pasta cresceu 2 pontos base
+4:12
+sempre next o que ele diz aqui é o
+4:15
+seguinte ele permite que você coloque
+4:17
+instaladores de ferramentas como o
+4:18
+wordpress o drupal dilma no caso aqui eu
+4:21
+não quero eu vou desmarcar essa opção
+4:22
+next mais uma vez e ele está dizendo que
+4:25
+vai começar a instalar next
+4:27
+a estação vai começar eventualmente em
+4:29
+algum desses espaços ou até nesse passo
+4:30
+que começou agora ele pode pedir algum
+4:32
+tipo de autorização por exemplo ele pode
+4:34
+pedir se olha só eu preciso de acesso à
+4:36
+pasta tal você autoriza você como
+4:38
+administrador autoriza você pode
+4:39
+autorizar porque o exemplo não é um
+4:41
+programa malicioso ele é um programa que
+4:43
+ele precisa de certos acessos já que
+4:45
+está transformando sua máquina um
+4:46
+servidor web então esse processo pode
+4:48
+levar alguns minutos na sua max varia de
+4:50
+computador o computador e a qualquer
+4:52
+momento aqui se ele parar
+4:53
+eu vou mostrar na minha tela que o que
+4:54
+pode acontecer mas dependendo de máquina
+4:57
+para máquina de windows para windows
+4:58
+ele pode solicitar em qualquer um dos
+5:00
+momentos uma autorização para instalar
+5:02
+um plugin para autorizar o acesso a uma
+5:04
+determinada pasta por exemplo pode
+5:07
+confiar
+5:07
+se você baixou diretamente desse site
+5:09
+que eu falei que o rapaz frentes não
+5:11
+baixa de outros sites
+5:12
+aí vou baixar de um fórum não basta
+5:15
+diretamente do site do zero original
+5:17
+então vai lá em apache franz ponto org e
+5:19
+baixo a última versão e instalando dessa
+5:21
+versão você pode confiar que é um
+5:23
+programa extremamente correto então a
+5:25
+instalação continua aqui você vai ver
+5:28
+aqui o exemplo é a pashimina mas que l
+5:30
+php e tem mais um p você pode ficar na
+5:33
+dúvida aproveitar que ele está
+5:34
+instalando aqui não está fazendo nada
+5:35
+vão continuar aula esse último psd
+5:37
+procura é uma linguagem do qual se você
+5:39
+fez o curso de php
+5:40
+o php se inspirou muito quando rasmus
+5:42
+lerdorf criou php
+5:44
+ele era programa do impro então muitos
+5:45
+comandos por exemplo como o eco do php é
+5:47
+um comando entrou o que você ainda não
+5:49
+fez o curso de php então aproveitar essa
+5:52
+semana vai ter caça ao leste ó aqui você
+5:54
+vai acessar o curso de php
+5:56
+você vai ter acesso a todas as aulas é
+5:58
+extremamente importante projeto que já
+5:59
+estava no ambiente já está aprendendo
+6:01
+banco de dados
+6:02
+aprendi php junto que com certeza depois
+6:04
+você vai conseguir fundir um no outro e
+6:06
+aí sim você verá um programador mais
+6:08
+experiente
+6:09
+então chegamos num momento em que ele
+6:10
+está finalizando a instalação até agora
+6:12
+no meu computador aqui e não pediu
+6:14
+autorização de nada e ele tá perguntando
+6:15
+aqui eu vou querer começar o control
+6:18
+penal contra o penãrol o painel de
+6:19
+controle do seu computador ou control
+6:20
+panel 200
+6:22
+vamos finalizar ele vai pedir que o
+6:23
+idioma e só tem a opção de inglês e
+6:26
+alemão eu acho que é melhor o inglês
+6:27
+clique em save e ele já abriu aqui o seu
+6:30
+painel de controle 200
+6:32
+se por acaso não há para não abrir aqui
+6:34
+não aparecer na tela é só você clica
+6:36
+aqui nesse quadradinho aqui você tem um
+6:38
+exemplo encontrou ele vai conseguir sair
+6:40
+duas vezes ele vai abrir perto ou abrir
+6:43
+ou fechar
+6:44
+se você fechar aqui não tem problema
+6:46
+clicando duas vezes aqui
+6:47
+eliabe por padrão o sempre vem com todos
+6:49
+os serviços desabitadas
+6:51
+vamos agora habilitar exatamente aquilo
+6:52
+que a gente precisa o que eu vou
+6:54
+precisar para esse curso é o apache para
+6:56
+isso você clique em start aqui na página
+6:58
+a primeira linha ficou mais clean ficou
+7:00
+verde já está funcionando
+7:01
+você vê aqui as portas que está
+7:03
+utilizando a pasta utilizando a porta 80
+7:05
+ea porta 443
+7:07
+vamos iniciar agora o mais que é ele que
+7:09
+vai utilizar a porta 33 06 exatamente
+7:11
+isso 3306 a porta padrão do mais que
+7:14
+elle e ele ficou verde
+7:15
+se por acaso ele não está aqui dá uma
+7:18
+olhada para ver se essas portas são
+7:19
+ocupadas por outro programa então o que
+7:21
+você tem que monitorar aqui são as
+7:22
+portas 80 a 443 para o apache ea 3306
+7:26
+por mais que é outras coisas que você
+7:28
+pode habilitar que são o suporte rafael
+7:30
+villa ao mercury e autocad que é um
+7:32
+interpretador jsp serve pra outra
+7:34
+linguagem no momento aqui eu vou só
+7:36
+colocar o apache e o mais que é ativo
+7:38
+se você quiser a qualquer momento
+7:39
+acessar o php mais de mim é que a
+7:41
+administração de banco de dados e mais
+7:43
+kelly
+7:43
+basta você acessar aqui ele já vai
+7:46
+operar mais de mim no nosso curso a
+7:48
+gente está utilizando o rock band do
+7:49
+mais que l
+7:50
+vamos abrir o rock band vamos fazer
+7:52
+funcionar no nosso servidor
+7:53
+eu vou te mostrar que funciona sem
+7:54
+problema algum então vou minimizar aqui
+7:56
+e vamos abrir o olho e dente
+8:01
+aqui ele vai carregar abrindo o
+8:05
+connection' se você vai ver que ele tem
+8:06
+uma conexão com o observa que uma
+8:08
+conexão que a gente já tinha feito com a
+8:10
+anp essa conexão não vai servir para o
+8:12
+seu exemplo a gente tem que ter uma
+8:13
+conexão nova por exemplo para fazer isso
+8:15
+basta você clicar aqui no mais em mim
+8:17
+ele vai perguntar
+8:18
+o endereço é 127 0 0 1 a porta 33 06
+8:22
+tudo bonitinho
+8:23
+vamos dar um nome para a conexão eu
+8:25
+voltar aqui mais que l 200 da ok ele já
+8:30
+tem aqui ó
+8:31
+vamos clicar em conectar ele deu um
+8:33
+aviso aqui de possibilidade de
+8:34
+incompatibilidades vou dar continuidade
+8:36
+e mesmo assim ele abriu aqui os meus
+8:39
+esquemas
+8:40
+se eu quiser eu posso vir aqui o botão
+8:42
+direito critica é uma só para testar
+8:44
+vou criar um meu banco
+8:47
+clique aqui embaixo e apply da ok
+8:50
+após mais uma vez e ele executou aqui
+8:54
+finalizar que eu já tenho o banco de
+8:56
+dados do meu banco
+8:57
+então agora eu tenho aqui o meu banco
+8:59
+criado aqui do lado esquerdo e ver que
+9:01
+ele já pode ser aberto e tudo vamos
+9:04
+fazer uma experiência aqui no zepp vamos
+9:07
+explicar em administração do banco de
+9:09
+dados está funcionando que ó eu tenho o
+9:11
+meu banco
+9:12
+tudo certinho funcionando sem problema
+9:14
+nenhum e vou aproveitar para mostrar a
+9:16
+você como é que você liga e desliga os
+9:17
+serviços e se realmente estão
+9:18
+funcionando
+9:19
+não vou abrir capital total do banco de
+9:21
+dados aberto vou fechar aqui a aba de
+9:23
+cima pra ele ficar abertos ao clicar
+9:25
+aqui ele abre um banco de dados é
+9:27
+normalmente o fechar de novo
+9:29
+fechei lá e vou vir aqui no sempre no
+9:32
+mais caritativo o desktop nele aqui só
+9:35
+vai sair o verdinho aqui já não está
+9:37
+mais verde significa que o servidor está
+9:39
+desligado
+9:40
+vou tentar ver agora eu mais quero
+9:42
+exemplo vai tentar abrir então vai
+9:44
+mostrar que não existe na o estatuto do
+9:46
+servidor que está parado e eu não posso
+9:48
+acessar
+9:49
+vou fechar novamente aqui vou voltar num
+9:52
+exemplo vou ativar o startac o processo
+9:55
+mais que l
+9:55
+vou abrir de novo aqui ele vai mostrar
+9:57
+uma mensagem continua eu já tenho um
+10:00
+banco de dados aqui funcionando sem
+10:02
+problema algum
+10:03
+e aí beleza experimento utilizar esse
+10:06
+processo no seu computador
+10:07
+obviamente vou repetir caso seu empenho
+10:10
+não funcione mas agora daqui pra frente
+10:11
+a gente vai continuar utilizando o mpe o
+10:13
+sempre é só uma possibilidade caso e não
+10:16
+funcione
+10:17
+então é isso que o gafanhoto continue
+10:19
+assistindo o curso em vídeo aqui você
+10:20
+tem acesso a tudo aquilo que a gente
+10:22
+fala sempre no final da aula a inscrição
+10:24
+o acesso a todas as aulas e o acesso ao
+10:26
+gol sem vídeo pontocom
+10:28
+não é só uma aula um pouco mais curta
+10:29
+onde eu quis mostrar somente o
+10:30
+funcionamento desse novo pacote que
+10:32
+transforma sua máquina no servidor e que
+10:34
+você vai poder utilizar caso você não
+10:36
+esteja conseguindo utilizar o é
+10:38
+e mais uma vez apenas relembrando você
+10:40
+também pode utilizar o sep caso seu
+10:42
+computador tenha linux ou mac o s
+10:44
+então é isso que nunca foi o tu eu
+10:45
+espero que agora tudo esteja funcionando
+10:47
+e que você possa fazer sua prática tem
+10:49
+muita gente reclamando que o elenco não
+10:50
+está funcionando
+10:51
+espero que agora você consiga botar em
+10:53
+prática finalmente todos os seus
+10:55
+conhecimentos que a gente vai aprender
+10:57
+durante esse curso
+10:58
+nunca deixe de praticar você sabe que
+11:00
+essa é a única forma de você conseguir
+11:01
+aprender alguma coisa
+11:02
+um forte abraço e até a próxima
+"""
