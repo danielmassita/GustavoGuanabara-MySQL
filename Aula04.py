95df0e103db5d5c20cc3bca26ee3251a47345d0e
Daniel Massita Tonolli <111195175+danielmassita@users.noreply.github.com>
2023-06-07 01:35:50 -0300

    Update Aula04.py

--- Aula04.py
+++ Aula04.py
@@ -6,5 +6,82 @@
 """
 Aula 04 - Curso MySQL #04 - Melhorando a Estrutura do Banco de Dados 
 
-Hello, world! 
+Hello, world! Vamos melhorar a estrutura do banco de dados. Basicamente, vamos refazer de uma maneira melhor (comandos de alter-table). 
+E lembra do bug? Múltiplos cadastros do seu Valdemir? Vamos corrigir!
+
+  Godofredo
+  32 anos
+  Masc
+  78.5 kg
+  1.83 m
+  Brasil
+  
+  Dolores
+  30 anos
+  Fem
+  52.3 kg
+  1.65 m
+  México
+  
+  Godolores
+  3 anos
+  Fem
+  25.8 kg
+  0.89 m
+  EUA
+
+Na aula passada, usamos os seguintes comandos:
+
+MySQL Workbench
+
+      SHOW databases;
+      CREATE DATABASE cadastro;
+      USE cadastro; 
+      CREATE TABLE pessoas (
+        nome VarChar(30),
+        idade TinyInt(3),
+        sexo Char(1),
+        peso Float,
+        altura Float,
+        nacionalidade VarChar(20)
+      ); 
+      DESCRIBE pessoas; 
 """
+
+# Vamos começar deletando o último BD chamado cadastro.
+DROP DATABASE cadastro;
+
+# Vamos criar um novo DB com dois PARÂMETROS (Constraints)
+CREATE DATABASE cadastro
+DEFAULT CHARACTER SET utf8
+DEFAULT COLLATE utf8_general_ci;
+
+# Vamos agora atualizar a ESTRUTURA da tabela, com campos mais inteligentes e tipos primitivos bem dimensionados... Mas já vamos OTIMIZAR a estrutura das tabelas.
+# Se um INT usa 11 bytes e TinyInt usa 3 bytes. Mas se falarmos de milhões de dados... ;)
+# Por exemplo, ao cadastrar a idade vamos usar o dia que nasceu, assim a tabela será dinâmica e atualizada sempre.
+
+CREATE TABLE pessoas (
+    `id` int NOT NULL AUTO_INCREMENT,
+    `nome` varchar(30) NOT NULL ,
+    `nascimento` date,
+    `sexo` enum('M', 'F'),
+    `peso` decimal(5,2),
+    `altura` decimal(3,2),
+    `nacionalidade` varchar(20) DEFAULT 'Brasil', 
+    primary key (id)
+) default charset = utf8;
+
+DESCRIBE pessoas;
+
+# Usamos duas constraints (not null e default).
+# Sempre importante ter um campo tipo CHAVE PRIMÁRIA (únicas).
+
+
+
+
+
+
+
+
+
+
