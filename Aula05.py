b7ee1eb01a21c2732f8cfe27934f36aa58c38458
Daniel Massita Tonolli <111195175+danielmassita@users.noreply.github.com>
2023-06-07 23:57:10 -0300

    Create Aula05.py

---
+++ Aula05.py
@@ -0,0 +1,3 @@
+# Curso MySQL #05 - Inserindo Dados na Tabela (INSERT INTO)
+# https://youtu.be/NCG9niOlm40
+# https://www.cursoemvideo.com/curso/mysql/aulas/banco-de-dados/modulos/inserindo-dados-na-tabela-insert-into/
