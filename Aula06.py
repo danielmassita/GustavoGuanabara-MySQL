b749de8ac7671c1d82c2795fcb23ee20eb99c795
Daniel Massita Tonolli <111195175+danielmassita@users.noreply.github.com>
2023-06-09 13:28:21 -0300

    Create Aula06.py

---
+++ Aula06.py
@@ -0,0 +1,10 @@
+# Curso MySQL #06 - Alterando a Estrutura da Tabela (ALTER TABLE e DROP TABLE)
+# https://youtu.be/To9qUcEMuY0
+# https://www.cursoemvideo.com/curso/mysql/aulas/banco-de-dados/modulos/alterando-a-estrutura-da-tabela-alter-table-e-drop-table/
+
+"""
+Aula 06 - Curso MySQL #06 - Alterando a Estrutura da Tabela (ALTER TABLE e DROP TABLE)
+
+
+"""
+
