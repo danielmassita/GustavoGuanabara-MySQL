bd1ee97f9c13685014c1f56881927543d38bb29a
Daniel Massita Tonolli <111195175+danielmassita@users.noreply.github.com>
2023-06-10 02:54:24 -0300

    Update Aula07.py

--- Aula07.py
+++ Aula07.py
@@ -25,8 +25,8 @@ INSERT INTO cursos VALUES
 ('3', 'Photoshop', 'Dicas de Photoshop CC', '10', '8', '2014'),
 ('4', 'PGP', 'Curso de PHP para Iniciantes', '40', '20', '2010'),
 ('5', 'Jarva', 'Introdução à Linguagem Java', '10', '29', '2000'),
-('6', 'MySQL', 'Bancos de Dados MySQL', '40', '37', '2014'),
-('1', 'HTML4', 'Curso de HTML5', '40', '37', '2014'),
-('1', 'HTML4', 'Curso de HTML5', '40', '37', '2014'),
-('1', 'HTML4', 'Curso de HTML5', '40', '37', '2014'),
-('1', 'HTML4', 'Curso de HTML5', '40', '37', '2014'),
+('6', 'MySQL', 'Bancos de Dados MySQL', '30', '15', '2016'),
+('7', 'Word', 'Curso Completo de Word', '40', '30', '2016'),
+('8', 'Sapateado', 'Danças Rítmicas', '40', '30', '2018'),
+('9', 'Cozinha Árabe', 'Aprenda a Fazer Kibe', '40', '30', '2018'),
+('10', 'YouTuber', 'Gerar Polêmica e Ganhar Inscritos', '5', '2', '2018');
