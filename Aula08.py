6e0c5c172d72bc234e56cfa1264276ab19f71f8b
Daniel Massita Tonolli <111195175+danielmassita@users.noreply.github.com>
2023-06-11 00:27:34 -0300

    Create Aula08.py

---
+++ Aula08.py
@@ -0,0 +1,5 @@
+# Curso MySQL #08 - Gerenciando Cópias de Segurança MySQL
+# https://youtu.be/w6OYS_M7hTM
+# https://www.cursoemvideo.com/curso/mysql/aulas/banco-de-dados/modulos/gerenciando-copias-de-seguranca-mysql/
+
+# GERENCIANDO CÓPIAS DE SEGURANÇA!
