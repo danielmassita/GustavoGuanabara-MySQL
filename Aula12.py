1ca6b9a094ce35555cbaaf67e9212e756215fe4b
Daniel Massita Tonolli <111195175+danielmassita@users.noreply.github.com>
2023-06-13 17:52:57 -0300

    Update Aula12.py

--- Aula12.py
+++ Aula12.py
@@ -508,3 +508,319 @@ ORDER BY nacionalidade ASC;
 +---------------+
 11 rows in set (0.02 sec)"""
 
+
+SELECT carga FROM cursos
+ORDER BY carga ASC;
+"""mysql> SELECT DISTINCT carga FROM cursos
+    -> ORDER BY carga ASC;
++-------+
+| carga |
++-------+
+|    10 |
+|    15 |
+|    20 |
+|    30 |
+|    35 |
+|    40 |
+|    50 |
+|    60 |
++-------+
+8 rows in set (0.00 sec)"""
+
+
+# FUNÇÕES DE AGREGAÇÃO 
+# Assim como Operadores Relacionais, FUNÇÕES de agregação são amplamente utilizadas, pra poder TOTALIZAR alguma coisa.
+
+
+SELECT COUNT(nome) FROM cursos;
+
+"""mysql> SELECT COUNT(nome) FROM cursos;
++-------------+
+| COUNT(nome) |
++-------------+
+|          30 |
++-------------+
+1 row in set (0.04 sec)"""
+
+
+SELECT COUNT(*) FROM cursos;
+
+"""mysql> SELECT COUNT(*) FROM cursos;
++----------+
+| COUNT(*) |
++----------+
+|       30 |
++----------+
+1 row in set (0.01 sec)"""
+
+
+SELECT * FROM cursos
+WHERE carga > 40;
+
+"""mysql> SELECT * FROM cursos
+    -> WHERE carga > 40;
++---------+-----------+------------------------------------------------------+-------+----------+------+
+| idcurso | nome      | descricao                                            | carga | totaulas | ano  |
++---------+-----------+------------------------------------------------------+-------+----------+------+
+|       9 | POO       | Curso de Programação Orientada a Objetos             |    60 |       35 | 2016 |
+|      14 | Android   | Curso de Desenvolvimento de Aplicativos para Android |    60 |       30 | 2018 |
+|      17 | Swift     | Curso de Desenvolvimento de Aplicativos para iOS     |    60 |       30 | 2019 |
+|      24 | WordPress | Curso de Criação de Sites com WordPress              |    60 |       30 | 2019 |
+|      25 | Joomla    | Curso de Criação de Sites com Joomla                 |    60 |       30 | 2019 |
+|      26 | Magento   | Curso de Criação de Lojas Virtuais com Magento       |    50 |       25 | 2019 |
++---------+-----------+------------------------------------------------------+-------+----------+------+
+6 rows in set (0.00 sec)"""
+
+
+SELECT COUNT(*) FROM cursos WHERE carga > 40;
+
+"""mysql> SELECT COUNT(*) FROM cursos WHERE carga > 40;
++----------+
+| COUNT(*) |
++----------+
+|        6 |
++----------+
+1 row in set (0.01 sec)"""
+
+
+SELECT * FROM cursos ORDER BY carga DESC;
+
+"""mysql> SELECT * FROM cursos ORDER BY carga DESC;
++---------+--------------------+------------------------------------------------------+-------+----------+------+
+| idcurso | nome               | descricao                                            | carga | totaulas | ano  |
++---------+--------------------+------------------------------------------------------+-------+----------+------+
+|       9 | POO                | Curso de Programação Orientada a Objetos             |    60 |       35 | 2016 |
+|      14 | Android            | Curso de Desenvolvimento de Aplicativos para Android |    60 |       30 | 2018 |
+|      17 | Swift              | Curso de Desenvolvimento de Aplicativos para iOS     |    60 |       30 | 2019 |
+|      24 | WordPress          | Curso de Criação de Sites com WordPress              |    60 |       30 | 2019 |
+|      25 | Joomla             | Curso de Criação de Sites com Joomla                 |    60 |       30 | 2019 |
+|      26 | Magento            | Curso de Criação de Lojas Virtuais com Magento       |    50 |       25 | 2019 |
+|       1 | HTML5              | Curso de HTML5                                       |    40 |       37 | 2014 |
+|       4 | PHP                | Curso de PHP para iniciantes                         |    40 |       20 | 2015 |
+|       5 | Java               | Introdução à Linguagem Java                          |    40 |       29 | 2015 |
+|       7 | Word               | Curso completo de Word                               |    40 |       30 | 2016 |
+|       8 | Python             | Curso de Python                                      |    40 |       18 | 2017 |
+|      10 | Excel              | Curso completo de Excel                              |    40 |       30 | 2017 |
+|      12 | C++                | Curso de C++ com Orientação a Objetos                |    40 |       25 | 2017 |
+|      19 | Redes              | Curso de Redes para Iniciantes                       |    40 |       15 | 2016 |
+|      29 | PHP7               | Curso de PHP, versão 7.0                             |    40 |       20 | 2020 |
+|      15 | JavaScript         | Curso de JavaScript                                  |    35 |       18 | 2017 |
+|       6 | MySQL              | Bancos de Dados MySQL                                |    30 |       15 | 2016 |
+|      11 | Responsividade     | Curso de Responsividade                              |    30 |       15 | 2018 |
+|      13 | C#                 | Curso de C#                                          |    30 |       12 | 2017 |
+|      16 | PowerPoint         | Curso completo de PowerPoint                         |    30 |       12 | 2018 |
+|      18 | Hardware           | Curso de Montagem e Manutenção de PCs                |    30 |       12 | 2017 |
+|      21 | SEO                | Curso de Otimização de Sites                         |    30 |       12 | 2017 |
+|      27 | Modelagem de Dados | Curso de Modelagem de Dados                          |    30 |       12 | 2020 |
+|      30 | PHP4               | Curso de PHP, versão 4.0                             |    30 |       11 | 2010 |
+|       2 | Algoritmos         | Lógica de Programação                                |    20 |       15 | 2014 |
+|      22 | Premiere           | Curso de Edição de Vídeos com Premiere               |    20 |       10 | 2017 |
+|      23 | After Effects      | Curso de Efeitos em Vídeos com After Effects         |    20 |       10 | 2018 |
+|      28 | HTML4              | Curso Básico de HTML, versão 4.0                     |    20 |        9 | 2010 |
+|      20 | Segurança          | Curso de Segurança                                   |    15 |        8 | 2018 |
+|       3 | Photoshop5         | Dicas de Photoshop CC                                |    10 |        8 | 2014 |
++---------+--------------------+------------------------------------------------------+-------+----------+------+
+30 rows in set (0.00 sec)"""
+
+
+SELECT max(carga) FROM cursos; # Seleciona o maior registro de todos os valores existentes em 'carga' dos cursos existentes.
+
+"""mysql> SELECT max(carga) FROM cursos;
++------------+
+| max(carga) |
++------------+
+|         60 |
++------------+
+1 row in set (0.00 sec)"""
+
+
+SELECT * FROM cursos WHERE ano = '2016'; # Seleciona todos os registros de cursos onde ano = 2016.
+
+"""mysql> SELECT * FROM cursos WHERE ano = '2016';
++---------+-------+------------------------------------------+-------+----------+------+
+| idcurso | nome  | descricao                                | carga | totaulas | ano  |
++---------+-------+------------------------------------------+-------+----------+------+
+|       6 | MySQL | Bancos de Dados MySQL                    |    30 |       15 | 2016 |
+|       7 | Word  | Curso completo de Word                   |    40 |       30 | 2016 |
+|       9 | POO   | Curso de Programação Orientada a Objetos |    60 |       35 | 2016 | <<<<<<<<<<<<<<<<<<
+|      19 | Redes | Curso de Redes para Iniciantes           |    40 |       15 | 2016 |
++---------+-------+------------------------------------------+-------+----------+------+
+4 rows in set (0.00 sec)"""
+
+SELECT max(totaulas) FROM cursos WHERE ano = '2016'; # Seleciona o maior valor do Total_Aulas dos cursos com ano = 2016.
+
+"""mysql> SELECT max(totaulas) FROM cursos WHERE ano = '2016';
++---------------+
+| max(totaulas) |
++---------------+
+|            35 |
++---------------+
+1 row in set (0.00 sec)"""
+
+
+SELECT min(totaulas) FROM cursos; # Seleciona o menor valor do Total_Aulas dos cursos existentes.
+
+"""mysql> SELECT min(totaulas) FROM cursos;
++---------------+
+| min(totaulas) |
++---------------+
+|             8 |
++---------------+
+1 row in set (0.00 sec)"""
+
+
+SELECT min(totaulas) FROM cursos WHERE ano = '2016'; # Seleciona o menor valor dos cursos existentes no ano de 2016.
+
+"""mysql> SELECT min(totaulas) FROM cursos WHERE ano = '2016';
++---------------+
+| min(totaulas) |
++---------------+
+|            15 |
++---------------+
+1 row in set (0.00 sec)"""
+
+
+SELECT nome, MIN(totaulas) FROM cursos WHERE ano = '2016';
+
+"""mysql> SELECT nome, MIN(totaulas) FROM cursos WHERE ano = '2016';
++-------+---------------+
+| nome  | MIN(totaulas) |
++-------+---------------+
+| MySQL |            15 |
++-------+---------------+
+1 row in set (0.00 sec)"""
+
+
+# Agora vamos para um detalhe da Função de Agregação. Elas retornarão APENAS o primeiro resultado da query, mas não todas.
+
+SELECT * FROM cursos WHERE ano = '2016';
+
+"""mysql> SELECT * FROM cursos WHERE ano = '2016';
++---------+-------+------------------------------------------+-------+----------+------+
+| idcurso | nome  | descricao                                | carga | totaulas | ano  |
++---------+-------+------------------------------------------+-------+----------+------+
+|       6 | MySQL | Bancos de Dados MySQL                    |    30 |       15 | 2016 |
+|       7 | Word  | Curso completo de Word                   |    40 |       30 | 2016 |
+|       9 | POO   | Curso de Programação Orientada a Objetos |    60 |       35 | 2016 |
+|      19 | Redes | Curso de Redes para Iniciantes           |    40 |       15 | 2016 |
++---------+-------+------------------------------------------+-------+----------+------+
+4 rows in set (0.00 sec)"""
+
+# Observe que MySQL tem 15 aulas e Redes também possui 15 aulas. 
+# A função de agregação capturou apenas a primeira ocorrência. 
+# Se eu quisesse mostrar os dois resultados, aí não seria somente FUNÇÃO DE AGREGAÇÃO, seria necessário também FUNÇÕES DE AGRUPAMENTO (próxima aula).
+
+# Agora vamos utilizar a Função de Agregação pra saber a SOMA (SUM).
+
+SELECT SUM(totaulas) FROM cursos;
+
+"""mysql> SELECT SUM(totaulas) FROM cursos;
++---------------+
+| SUM(totaulas) |
++---------------+
+|           583 |
++---------------+
+1 row in set (0.00 sec)"""
+
+
+SELECT sum(totaulas) FROM cursos WHERE ano = '2016';
+
+"""mysql> SELECT sum(totaulas) FROM cursos WHERE ano = '2016';
++---------------+
+| sum(totaulas) |
++---------------+
+|            95 |
++---------------+
+1 row in set (0.00 sec)"""
+
+
+# Agora vamos utilizar a Função de Agregação pra saber a MÉDIA (AVG). 
+
+SELECT AVG(totaulas) FROM cursos;
+"""mysql> SELECT AVG(totaulas) FROM cursos;
++---------------+
+| AVG(totaulas) |
++---------------+
+|       19.4333 |
++---------------+
+1 row in set (0.01 sec)"""
+
+
+
+
+# LISTA DE EXERCÍCIOS DAS AULAS 11 e 12 - Exercitando! (importar a dump da aula anterior)
+
+# 1 - Uma lista com o nome de todas as gafanhotas.
+# 2 - Uma lista com os dados de todos aqueles que nasceram entre 1-Jan-2000 e 31-Dez-2015.
+# 3 - Uma lista com o nome de todos os homens que trabalham como Programador.
+# 4 - Uma lista com os dados de todas as mulheres que nasceram no Brasil e que têm seu nome iniciando com a letra J.
+# 5 - Uma lista com o nome e a nacionalidade de todos os homens que têm Silva no nome, não nasceram no Brasil e pesam menos de 100 kg.
+# 6 - Qual é a maior altura entre gafanhotos homens brasileiros?  
+# 7 - Qual é a média de peso dos gafanhotos cadastrados?
+# 8 - Qual o menor peso entre as gafanhotas mulheres que nasceram fora do Brasil entre 01-Jan-1990 e 31-Dez-2000?
+# 9 - Quantas gafanhotas mulheres tem mais de 1.90 m de altura?
+
+SHOW DATABASES;
+USE cadastro;
+SHOW TABLES;
+DESCRIBE gafanhotos;
+"""mysql> describe gafanhotos;
++---------------+---------------+------+-----+---------+----------------+
+| Field         | Type          | Null | Key | Default | Extra          |
++---------------+---------------+------+-----+---------+----------------+
+| id            | int           | NO   | PRI | NULL    | auto_increment |
+| nome          | varchar(30)   | NO   |     | NULL    |                |
+| profissao     | varchar(20)   | YES  |     | NULL    |                |
+| nascimento    | date          | YES  |     | NULL    |                |
+| sexo          | enum('M','F') | YES  |     | NULL    |                |
+| peso          | decimal(5,2)  | YES  |     | NULL    |                |
+| altura        | decimal(3,2)  | YES  |     | NULL    |                |
+| nacionalidade | varchar(20)   | YES  |     | Brasil  |                |
++---------------+---------------+------+-----+---------+----------------+
+8 rows in set (0.00 sec)"""
+
+
+# 1 - Uma lista com o nome de todas as gafanhotas.
+
+SELECT nome FROM gafanhotos WHERE sexo = 'F' ORDER BY nome ASC;
+"""mysql> SELECT nome FROM gafanhotos WHERE sexo = 'F' ORDER BY nome;
++------------------------+
+| nome                   |
++------------------------+
+| Ana Carolina Hernandes |
+| Ana Carolina Mendes    |
+| Andreia Delfino        |
+| Bruna Santos           |
+| Bruna Teles            |
+| Daniele Moledo         |
+| Dayana Dias            |
+| Elaine Nunes           |
+| Janaína Couto          |
+| Janaina Oliveira       |
+| Jarismar Andrade       |
+| Josiane Dutra          |
+| Jucinei Teixeira       |
+| Karine Ribeiro         |
+| Leila Martins          |
+| Letícia Neves          |
+| Monique Precivalli     |
+| Nara Matos             |
+| Rita Pontes            |
+| Rosana Kunz            |
+| Silvana Albuquerque    |
+| Talita Nascimento      |
++------------------------+
+22 rows in set (0.00 sec)"""
+
+
+# 2 - Uma lista com os dados de todos aqueles que nasceram entre 1-Jan-2000 e 31-Dez-2015.
+SELECT * FROM gafanhotos WHERE nascimento
+
+
+# 3 - Uma lista com o nome de todos os homens que trabalham como Programador.
+# 4 - Uma lista com os dados de todas as mulheres que nasceram no Brasil e que têm seu nome iniciando com a letra J.
+# 5 - Uma lista com o nome e a nacionalidade de todos os homens que têm Silva no nome, não nasceram no Brasil e pesam menos de 100 kg.
+# 6 - Qual é a maior altura entre gafanhotos homens brasileiros?  
+# 7 - Qual é a média de peso dos gafanhotos cadastrados?
+# 8 - Qual o menor peso entre as gafanhotas mulheres que nasceram fora do Brasil entre 01-Jan-1990 e 31-Dez-2000?
+# 9 - Quantas gafanhotas mulheres tem mais de 1.90 m de altura?
