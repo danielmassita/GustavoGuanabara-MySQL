a99e27f50d880dc08ff964f185069dd4eed7b02b
Daniel Massita Tonolli <111195175+danielmassita@users.noreply.github.com>
2023-06-14 14:05:36 -0300

    Update Aula14.py

--- Aula14.py
+++ Aula14.py
@@ -1,3 +1,20 @@
 # Curso MySQL #14 - Modelo Relacional
 # https://youtu.be/8fxKJWJcRTw
 # 
+
+""" 
+RELACIONAMENTO ENTRE TABELAS ou MODELO RELACIONAL
+- Na década de 60, o Gov. EUA e IBM, fizeram um evento CODASIL onde nasceu o COBOL.
+- Na época, surgiram os modelos hierárquicos e em rede, mas aos longo dos anos ficou ineficiente.
+- Na década de 70, O Edgar F. Cood (Eng. IBM) pensou um modelo diferenciado, DADOS (muito mais que apenas ligações), eles teriam relações entre eles.
+- Surgiu assim o Modelo Relacional.
+
+- Foi criado relaçõess, sendo várias ligações (não apenas uma), e com significado.
+- Hoje em dia, um cadastro de uma pessoa, eu tenho um registro.
+- A partir dessa 'pessoa', podemos saber onde ela mora, produtos comprados, baixa de estoque, sistema financeiro, pedido de reposição, informação entrega, etc.
+
+- Novos modelos surigram, como o MODELO ORIENTADO A OBJETO, mas o MODELO RELACIONAL ainda é muito usado e presente.
+- Vamos voltar no nosso exemplo.
+
+
+"""
