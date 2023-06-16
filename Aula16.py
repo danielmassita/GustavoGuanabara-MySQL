a07c1c547da7b7e7cacfd8630bb54f3a2639b404
Daniel Massita Tonolli <111195175+danielmassita@users.noreply.github.com>
2023-06-16 01:32:54 -0300

    Create Aula16.py

---
+++ Aula16.py
@@ -0,0 +1,3 @@
+# Curso MySQL #16 - INNER JOIN com várias tabelas
+# https://youtu.be/jx2ne8iZMOA
+# https://www.cursoemvideo.com/curso/mysql/aulas/banco-de-dados/modulos/inner-join-com-varias-tabelas/
